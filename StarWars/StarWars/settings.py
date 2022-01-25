90928ec9a0c7a63191c808db46913db7f3f9972d
maximF39 <maks.fedotow14@mail.ru>
2022-01-25 22:48:48 +0500

    init brach test
    pass

--- StarWars/StarWars/settings.py
+++ StarWars/StarWars/settings.py
@@ -4,15 +4,12 @@ from pathlib import Path
 from django.template.context_processors import request
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 SECRET_KEY = 'django-insecure-6$t@4ws7b#l=*2%nw8z0ycx#+ppss)bk&ku+rr6bt+l=xw-zfl'
-
 DEBUG = True
-
 ALLOWED_HOSTS = []
-
 INSTALLED_APPS = [
     'Main.apps.MainConfig',
+    'Forum.apps.ForumConfig',
     'PersonalAccount.apps.PersonalaccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -21,8 +18,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lineage',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -32,9 +31,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'StarWars.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -50,18 +47,13 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'StarWars.wsgi.application'
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -76,26 +68,17 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 LANGUAGE_CODE = 'ru'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
--- StarWars/StarWars/urls.py
+++ StarWars/StarWars/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
+import rest_framework
+import djoser
 
 urlpatterns = [
-    path('', include('Main.urls')),
-    path('account/', include('PersonalAccount.urls')),
+    path('api/v1/base-auth/', include('rest_framework.urls')),
+    path('api/v1/main/', include('Main.urls')),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth_token/', include('djoser.urls.authtoken')),
+    # path('', include('Main.urls')),
+    # path('forum/', include('Forum.urls')),
+    # path('account/', include('PersonalAccount.urls')),
     path('admin/', admin.site.urls),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
