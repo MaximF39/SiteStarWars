e9d2d00b41de3f53338f21139c160cca01e50405
Алексей Карпов <73760173+LastLow098@users.noreply.github.com>
2022-01-27 01:39:51 +0300

    seccion
    lexa seccions

--- StarWars/Main/static/Main/js/func.js
+++ StarWars/Main/static/Main/js/func.js
@@ -7,3 +7,4 @@ function showpass(id) {
     }
   }
  
+
--- StarWars/PersonalAccount/static/PersonalAccount/js/func.js
+++ StarWars/PersonalAccount/static/PersonalAccount/js/func.js
@@ -1,10 +1,16 @@
-function showpass(id) {
-    element = document.getElementById(id);
-    if (element.type === "password") {
-      element.type = "text";
-    } else {
-      element.type = "password";
-    }
+// function showpass(id) {
+//     element = document.getElementById(id);
+//     if (element.type === "password") {
+//       element.type = "text";
+//     } else {
+//       element.type = "password";
+//     }
+//   }
+
+document.querySelector('.paneltop').onclick = function(event) {
+  for(let i = 1; i <=6; ++i) {
+    document.querySelector('.lk'+i).style.display = "none";
   }
 
- 
+  document.querySelector('.'+event.target.id).style.display = "block";
+}
