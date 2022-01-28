16d6d6ab02d914095d0d33aa2ca7fc9a5fc19eda
maximF39 <maks.fedotow14@mail.ru>
2022-01-28 22:25:38 +0500

    fixed position elements

--- StarWars/PersonalAccount/static/PersonalAccount/js/func.js
+++ StarWars/PersonalAccount/static/PersonalAccount/js/func.js
@@ -1,16 +1,11 @@
-// function showpass(id) {
-//     element = document.getElementById(id);
-//     if (element.type === "password") {
-//       element.type = "text";
-//     } else {
-//       element.type = "password";
-//     }
-//   }
-
 document.querySelector('.paneltop').onclick = function(event) {
+  if (event.target.id == 0){
+    return;
+  }
   for(let i = 1; i <=6; ++i) {
     document.querySelector('.lk'+i).style.display = "none";
   }
 
   document.querySelector('.'+event.target.id).style.display = "block";
 }
+//need use addEventListener
--- StarWars/StarWars/urls.py
+++ StarWars/StarWars/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import include, path
+import rest_framework
+import djoser
+
 
 urlpatterns = [
     path('', include('Main.urls')),
     path('forum/', include('Forum.urls')),
     path('account/', include('PersonalAccount.urls')),
     path('admin/', admin.site.urls),
+
+    path('api/v1/base-auth/', include('rest_framework.urls')),
+    path('api/v1/main/', include('Main.urls')),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth_token/', include('djoser.urls.authtoken')),
 ]
