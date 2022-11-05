53bec0ae4d2030e546e8981d4e3c1728d984e2f9
alexander.iii@mail.ru <alexander.iii@mail.ru>
2022-11-05 18:22:45 +0600

    Стилизация фильтра

--- backend/apps/shop/views.py
+++ backend/apps/shop/views.py
@@ -3,12 +3,12 @@ from . import models
 
 
 def shop(request):
-    if request.method == 'POST' and int(request.POST['filter']) != 8:
+    if request.method == 'POST' and int(request.POST['filter']) != 1:
         id_type = int(request.POST['filter'])
         en_name = models.ItemsType.objects.values_list('type_en', flat=True).get(id=id_type)
         items = getattr(models, en_name).objects.all()
     else:
-        id_type = 8
+        id_type = 1
         items = models.BaseItems.objects.select_related('ammo', 'resources', 'engines', 'devices', 'weapons', 'droids', 'ships')
     filter = models.ItemsType.objects.all()
     return render(request, 'shop/shop.html', context={'items': items, 'filter': filter, 'type': id_type})
