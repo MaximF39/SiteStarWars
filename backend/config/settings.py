6c01aee00d49930c2658025b1649ab142cbcab07
maxf39 <maks.fedotow14@mail.ru>
2022-11-06 01:13:06 +0500

    release 0.96

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'test88',#os.getenv("POSTGRES_DB"),
+        "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
--- docker/nginx/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-apt-get update
-sudo apt-get install certbot
-apt-get install python-certbot-nginx
-nginx -t && nginx -s reload
-
-certbot --nginx -d serverstarwars.site -d www.serverstarwars.site
