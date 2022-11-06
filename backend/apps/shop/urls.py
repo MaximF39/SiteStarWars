3d29ed7ed5505847e87b8cd49732eed143f1602f
alexander.iii@mail.ru <alexander.iii@mail.ru>
2022-11-06 16:18:16 +0600

    Преобразовал функцию в класс

--- backend/apps/shop/urls.py
+++ backend/apps/shop/urls.py
@@ -6,6 +6,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', shop, name='shop'),
+    path('', shop.as_view(), name='shop'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- backend/apps/shop/views.py
+++ backend/apps/shop/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.http import HttpResponse
+from .models import *
 from . import models
 
 
-def shop(request):
-    types = models.ItemsType.objects.all()
-    select_type = int(request.GET.get('item_type', 1))
-    en_name = types[select_type - 1].type_en
-    if select_type == 1:
-        items = models.BaseItems.objects.select_related('ammo', 'resources', 'engines', 'devices', 'weapons', 'droids', 'ships')
-    else:
-        items = getattr(models, en_name).objects.all()
-    return render(request, 'shop/shop.html', context={'items': items, 'types': types, 'select_type': select_type})
+class shop(ListView):
+    model = BaseItems
+    template_name = 'shop/shop.html'
+    object_list = 'items'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get(self, request):
+        select_type = int(request.GET.get('item_type', 1))
+        types = models.ItemsType.objects.all()
+        en_name = types[select_type - 1].type_en
+        if select_type == 1:
+            items = models.BaseItems.objects.select_related('ammo', 'resources', 'engines', 'devices', 'weapons', 'droids', 'ships')
+        else:
+            items = getattr(models, en_name).objects.all()
+        return render(request, self.template_name, context={'items': items, 'types': types, 'select_type': select_type})
