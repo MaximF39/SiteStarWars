39aeb8e3949ffeb977dd20501102d3a6357fca35
alexander.iii@mail.ru <alexander.iii@mail.ru>
2022-11-06 21:20:43 +0600

    Добавил пагинацию

--- backend/apps/shop/views.py
+++ backend/apps/shop/views.py
@@ -7,7 +7,7 @@ from . import models
 
 
 class Shop(ListView):
-    model = BaseItems
+    paginate_by = 10
     template_name = 'shop/shop.html'
     context_object_name = 'items'
     object_list = 'items'
@@ -20,8 +20,13 @@ class Shop(ListView):
         context['types'] = types
         return context
 
+    def get_queryset(self):
+        model = BaseItems.objects.all().order_by('id')
+        return model
+
 
 class ShopFilter(ListView):
+    paginate_by = 10
     template_name = 'shop/shop.html'
     context_object_name = 'items'
 
@@ -39,8 +44,6 @@ class ShopFilter(ListView):
         en_name = types[select_type - 1].type_en
         model_items = getattr(models, en_name).objects.all().order_by('id')
         return model_items
-
-
 
 
 def redirect_shop(request):
